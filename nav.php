0ddcc0b2b2353f8d00828ad894d40a01e2c6f2e7
相澤春伽 <aizawadesign13@gmail.com>
2020-07-25 13:12:03 +0900

    HTML \CS

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav class="grobal-nav">
+    <a href="http://mov.team"><img class="grobal-nav_logo" src="assets/img/logomark.svg" alt="サイトのトップに遷移します。"></a>
+    <div class="grobal-nav_container">
+      <ul class="grobal-nav_link-top-container">
+          <li class="grobal-nav_link"><a href="#about">about</a></li>
+          <li class="grobal-nav_link"><a href="#member">member</a></li>
+          <li class="grobal-nav_link"><a href="works/">works</a></li>
+          <li class="grobal-nav_link"><a href="contact/">contact</a></li>
+      </ul>
+    </div>
+</nav>
