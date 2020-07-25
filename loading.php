8dc4d55d6bf9278c5b7bdd0c6c0e584872905800
相澤春伽 <aizawadesign13@gmail.com>
2020-07-25 13:23:00 +0900

    Revert "HTML \CS"
    This reverts commit 0ddcc0b2b2353f8d00828ad894d40a01e2c6f2e7.

--- loading.php
+++
@@ -1,4 +0,0 @@
-<div class="loading_container is-loading">
-    <img src="assets/img/loading.svg" alt="now loading" class="loading_img">
-    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
-</div>
